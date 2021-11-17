7de1d219bd62503c5e0a4d9ec0aa0fa8759cca1e
Flávio Pereira <fpereirasilv@gmail.com>
2021-11-17 19:34:27 -0300

    Criação de Componentes ReactJs

---
+++ react-dio/src/App.js
@@ -0,0 +1,16 @@
+
+const App = () => {
+  return(
+    <h1>Hello World</h1>
+  )
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello Word</h1>
+//     </div>
+//   );
+// }
+
+export default App;
